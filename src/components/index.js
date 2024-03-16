b0313d8f2cc7f948560a38eeeae015c16c39a98e
Aira Jena <airajena.hack@gmail.com>
2024-03-16 13:11:33 +0530

    feat: Added emailjs functionality in the conatct form and Fox animation

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import HomeInfo from "./HomeInfo"
 import Loader from "./Loader"
+import Alert from "./Alert";
 export {
     Navbar,
     HomeInfo,
-    Loader
+    Loader,
+    Alert
 }
