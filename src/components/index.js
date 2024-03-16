08b4f7f013ebdbbf878480c851c693ff41454727
Aira Jena <airajena.hack@gmail.com>
2024-03-17 00:00:04 +0530

    feat: Built the about page with dummy data for experience and skills

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Navbar from "./Navbar";
 import HomeInfo from "./HomeInfo"
 import Loader from "./Loader"
 import Alert from "./Alert";
+import CTA from "./CTA";
 export {
     Navbar,
     HomeInfo,
     Loader,
-    Alert
+    Alert,
+    CTA
 }
