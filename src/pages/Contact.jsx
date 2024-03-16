8fb0edba25a3001e30936010bd0cff68cdc3d872
Aira Jena <airajena.hack@gmail.com>
2024-03-16 10:10:14 +0530

    feat: Built the Contact Form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import {useRef, useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
 
-export default Contact
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+
+  return (
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-7 mt-14'
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows='4'
+              className='textarea'
+              placeholder='Write your thoughts here...'
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type='submit'
+            disabled={loading}
+            className='btn'
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
